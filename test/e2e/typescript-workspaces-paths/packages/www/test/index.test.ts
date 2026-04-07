28294660672febe2a0d8834934471a97bcb1232c
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-04-07 18:53:48 +0200

    Test typescript-workspaces-paths with and without baseUrl

--- test/e2e/typescript-workspaces-paths/packages/www/test/index.test.ts
+++ test/e2e/typescript-workspaces-paths/packages/www/test/index.test.ts
@@ -1,14 +1,62 @@
 /* eslint-env jest */
 
 import { join } from 'path'
-import { nextTestSetup } from 'e2e-utils'
+import { nextTestSetup, isNextDev } from 'e2e-utils'
+import { readFileSync, writeFileSync } from 'fs'
 
 describe('TypeScript Features', () => {
-  describe('default behavior', () => {
+  describe.each([
+    { label: '', testBaseUrl: true },
+    { label: ' without baseUrl', testBaseUrl: false },
+  ])('default behavior$label', ({ testBaseUrl }) => {
+    const tsConfigPath = join(__dirname, '../../../tsconfig.json')
+    let previousTsConfigJsonc: string | null = null
+    beforeAll(async () => {
+      if (testBaseUrl) {
+        previousTsConfigJsonc = readFileSync(tsConfigPath, 'utf-8')
+
+        const nextTsConfigJsonc = previousTsConfigJsonc
+          .replace('// baseUrl will go here', '"baseUrl": ".",')
+          .replace(
+            './packages/www/types/unfetch.d.ts',
+            'packages/www/types/unfetch.d.ts'
+          )
+          .replace('./packages/www/components/*', 'packages/www/components/*')
+          .replace('./packages/lib/a/*', 'packages/lib/a/*')
+          .replace('./packages/lib/b/*', 'packages/lib/b/*')
+          .replace(
+            './packages/www/components/hello.tsx',
+            'packages/www/components/hello.tsx'
+          )
+          .replace(
+            './packages/www/components/alias-to-d-ts.d.ts',
+            'packages/www/components/alias-to-d-ts.d.ts'
+          )
+          .replace(
+            './packages/www/components/alias-to-d-ts.tsx',
+            'packages/www/components/alias-to-d-ts.tsx'
+          )
+        writeFileSync(tsConfigPath, nextTsConfigJsonc)
+      }
+    })
+
+    afterAll(() => {
+      if (previousTsConfigJsonc !== null) {
+        writeFileSync(tsConfigPath, previousTsConfigJsonc)
+      }
+    })
+
     const { next } = nextTestSetup({
       skipDeployment: true,
+      dependencies: testBaseUrl
+        ? {
+            typescript: '5.9.3',
+          }
+        : undefined,
       files: join(__dirname, '../../../'),
-      subDir: 'packages/www',
+      buildCommand: 'pnpm next build packages/www',
+      startCommand:
+        'pnpm next ' + (isNextDev ? 'dev' : 'start') + ' packages/www',
     })
 
     it('should alias components', async () => {
